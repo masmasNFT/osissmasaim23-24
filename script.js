5c483d38d3d4675eb67ddc7f3c6c27a38ed66e52
masmasNFT <98086588+masmasNFT@users.noreply.github.com>
2023-09-11 18:52:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,26 @@ function cekPenerimaan() {
         "mohammad ikhsan ramadhan"
     ];
 
+    const namaNgapain = [
+        "anarghya dedari imansyah",
+        "aulia nadya azizah",
+        "kalayya mahdialika fahmi",
+        "neiva pelangi rinjani",
+        "ratu rayyani",
+        "keisha luana putri nugroho",
+        "muhammad hervian bumi orfala",
+        "muhammad hafizh fauzan",
+        "naufal aqila hamzah",
+        "reza ali muhammad"
+    ];
+
     const namaDiterima2Kata = namaDiterima.map(nama => nama.split(' ').slice(0, 2).join(' '));
+    const namaNgapain2Kata = namaNgapain.map(nama => nama.split(' ').slice(0, 2).join(' '));
 
     if (namaDiterima2Kata.includes(kataInput)) {
         window.location.href = "halaman-diterima.html"; // Arahkan ke laman kedua
+    } else if (namaNgapain2Kata.includes(kataInput)) {
+        window.location.href = "halaman-ngapain.html"; // Arahkan ke laman "Ngapain"
     } else {
         window.location.href = "halaman-tidak-diterima.html"; // Arahkan ke laman "Tidak Diterima"
     }
